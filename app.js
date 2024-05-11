21d376b54a7e475d3d3b54139a230e5a4da76be9
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-05-11 22:04:10 +0500

    final code

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 function randomQuotesGenerator() {
-  let quotes = [
-    "Dil ki baat chhupana aasan nahi hota,Har koi khushnaseeb nahi hota,Pyar to sabko milta hai zindagi mein,Magar har koi aap jaisa dost nahi hota.",
-    "Mohabbat mein har khushi aam nahi hoti,Har muskurahat pyaar ki shaan nahi hoti,Kuch log samajh paate hain ishq ko,Magar har dilwala asaan nahi hota.",
-    "Dard ki gehraaiyon se guzarna hai mujhe,Muskurana seekha hai, rona nahi seekha,Zindagi ke safar mein saathi chahiye,Dosti ka asli matlab samjha nahi seekha.",
-    "Raat guzri phir mahekti subah aayi,Dil dhadka phir se yaad teri aayi,Zindagi haseen hai, phir bhi tanhai hai,Kyunki teri dosti ki kami har pal mehsoos aayi.",
-    "Mohabbat karke dekho, dil ko aaraam milega,Pyar karne se dil ko sukoon milega,Mushkil hai pyar ko bhulana,Kyunki har kadam par tera hi saath chahiye.",
-    "Khushbu teri yaadon ki mere dil ko behlaati hai,Tere saath bita har lamha yaadgaar hota hai,Mohabbat mein teri hamesha mere dil mein rahegi,Kyunki tere bina meri zindagi adhoori hoti hai.",
-    "Dil ki baat dil mein na rakhna,Aankhon se aankhon ki baat kehna,Dost toh har koi bana sakta hai,Magar dosti nibhana mushkil hota hai.",
-    "Dil ki duniya mein koi gham na ho,Khuda kare zindagi mein koi kami na ho,Har pal pyar ka ehsaas ho tumhe,Dosti ka saath ho aur yaadon ka sahara ho tumhe.",
-    "Tanhaiyon mein bhi tera saath chahiye,Muskurahaton mein bhi tera saath chahiye,Zindagi mein har kadam par yaad aaye tu,Kyunki teri dosti hi meri zindagi ka asli maqsad hai.",
-    "Dosti ka rishta anokha hai,Na gham ke liye na rone ke liye,Zindagi mein har pal dosti ka ehsaas ho,Kyunki dosti hi sabse khoobsurat rishta hai."];
-  
-    let randomQuotes = Math.random() * quotes.length;
-    let roundQuotes = Math.floor(randomQuotes);
-    let quotesGen = quotes[roundQuotes];
-    
-    let random_Quotes =document.querySelector('#quotes');
-    random_Quotes.innertext = `" ${quotesGen} " `;
-  }
+  let random_quotes = [
+    "zindagi hai ya koi toofan hai",
+    "zindagi jabre musalsal ki tarah kaati hai jaane kis jurm ki paayi hai saza yaad nahi. (Saghar Siddiqui)",
+    "zindagi riyazi ka sawal nahi jis ka jawab maloom ho sake.(Wasif Ali Wasif)",
+    "zindagi khud raaste banati hai, raaste zindagi nahi banate. (Saadat Hasan Manto)",
+    "zara num ho to yeh matti bari zarkhez hai saqi.(Allama Iqbal)",
+    "badhi manzilon ke musafir chota dil nahi rakhte. (Wasif Ali Wasif)",
+    "insaan ke liye wohi kuch hai jis ke liye wo koshish kare. (Al-Quraan)",
+    "Aqal mand apne aib ko khud dekhta hai aur bewaqoof ka aaeb dunya dekhti hai. (Sheikh Saadi)",
+    "museebat agar insaan ko dolatmand nahi to aqal mand zaroor bana deti hai.",
+    "hum taleem khareed sakte hain lekin aqal khuda ka ata karda tohfa hai. (Saadat Hasan Manto)",
+    "Aqal mand hai woh shakhs jo anjaam soch kar kaam kare. (Hazrat Ali).",
+    "waqt acha bhi aaye gaa nasir gham na kar zindagi parhi hai abhi. (Nasir Kazmi)",
+    "waqt to waqt per badalta hai lekin insaan kisi bhi waqt badal sakta hai.",
+    "mushkil waqt sabhi per aata hai koi bikhar jaata hai koi nikhar jaata hai",
+    "sitaron se aagey jahan aur bhi hain abhi ishq ke imtihaan aur bhi hain. (Allama Iqbal)",
+    "mangi hui mohabbat ka maza bigdhi hui sharaab jaisa hota hai. (Bano Qudsia)"
+  ];
+
+  let randomQuotes = Math.random() * random_quotes.length;
+  let roundQuotes = Math.floor(randomQuotes);
+  let quotesGen = random_quotes[roundQuotes];
+
+  let quote = document.querySelector("#quotes");
+  quote.innerText = `" ${quotesGen} "`;
+}
